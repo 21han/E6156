c6f1536a309a9f6e09ddb5474b2f7c2d4f4bedd1
EC2 Default User <ec2-user@ip-172-31-41-1.ec2.internal>
2020-10-14 02:17:51 +0000

    fixed typo

--- application.py
+++ application.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-from flask import Flask
-app = Flask(__name__)
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -21,3 +18,8 @@ def profile(username):
 @app.route('/login_test/')
 def hello(name=None):
     return render_template('login.html')
+
+
+
+
+

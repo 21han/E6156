631592c72baaebdb3891f7d46ef079dd6e15b647
EC2 Default User <ec2-user@ip-172-31-41-1.ec2.internal>
2020-10-14 04:09:00 +0000

    backup

--- connection.py
+++ connection.py
@@ -1,11 +1,19 @@
 
 import pymysql
+import json
 
 c_info = {
     "host": "user-service-db.ci3ta0leimzm.us-east-2.rds.amazonaws.com",
     "user": "admin",
     "password": "12345678",
-    "cursorclass": pymysql.cursor.DictCursor,
+    "cursorclass": pymysql.cursors.DictCursor,
 }
 
 conn = pymysql.connect(**c_info)
+
+cur = conn.cursor()
+res = cur.execute("show databases;")
+res = cur.fetchall()
+
+print(f"database: {json.dumps(res, indent=4, default=str)}")
+

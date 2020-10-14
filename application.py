5d20dc420bfeb776b5329e6aadcabc3d8d9373a5
Zhan Zhicheng <zhanzhicheng@Zhans-MacBook-Pro.local>
2020-10-13 23:54:35 -0500

    add|finish get method for address and user

--- application.py
+++ application.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, Response
+import pymysql
+import json
 
 app = Flask(__name__)
+
+c_info = {
+    "host": "user-service-db.ci3ta0leimzm.us-east-2.rds.amazonaws.com",
+    "user": "admin",
+    "password": "12345678",
+    "cursorclass": pymysql.cursors.DictCursor,
+}
+
+conn = pymysql.connect(**c_info)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+
 @app.route('/login')
 def login():
     return 'login'
+
 
 @app.route('/user/<username>')
 def profile(username):
@@ -20,11 +34,33 @@ def hello(name=None):
     return render_template('login.html')
 
 
+@app.route('/Users/<id>', methods=['GET'])
+def get_users_by_id(id):
+    sql = """SELECT * FROM rds_user_service.users where id = {}""".format(id)
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute(sql)
+            rsp = Response(json.dumps(cursor.fetchall()), status=200, content_type="application/json")
+            return rsp
+        except (pymysql.Error, pymysql.Warning) as e:
+            print(f'error! {e}')
+
+
+@app.route('/Addresses/<id>', methods=['GET'])
+def get_addresses_by_id(id):
+    sql = """SELECT * FROM rds_user_service.address where id = {}""".format(id)
+    with conn.cursor() as cursor:
+        try:
+            cursor.execute(sql)
+            rsp = Response(json.dumps(cursor.fetchall()), status=200, content_type="application/json")
+            return rsp
+        except (pymysql.Error, pymysql.Warning) as e:
+            print(f'error! {e}')
+
+
 def main():
     app.run(debug=True, threaded=True, host='0.0.0.0', port='5000')
 
 
 if __name__ == "__main__":
     main()
-
-

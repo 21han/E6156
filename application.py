1e4284b3fd8dc64871d9df17da191a7b957a11d3
Zihan Guo <zihan@Zihans-MBP.fios-router.home>
2020-10-13 22:10:44 -0400

    add other files

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+
+@app.route('/login_test/')
+def hello(name=None):
+    return render_template('login.html')
